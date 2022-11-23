877c0e8221496e1b6bb62c30f582fd8ad3f5e449
Siobhantt <luisa.cardozo@iesnervion.es>
2022-11-23 12:25:32 +0100

    (:

--- PracticaFor/src/practicafor/Guarreo.java
+++ PracticaFor/src/practicafor/Guarreo.java
@@ -1,16 +1,35 @@
 package practicafor;
 
+import java.util.Scanner;
+
 public class Guarreo {
 
 	public static void main(String[] args) {
+	    
+		int tamaño = 5;
 		
-		for(int i=1; i<=10; i++) {
-			
-			
-			System.out.println(i);
-			
-			
+		for (int i =1; i<=tamaño; i++) {
+	    	System.out.print("*");
+	    }
+	    System.out.println("");
+	    
+	    
+	    for(int medio=1; medio <= tamaño-2; medio++) {
+	    System.out.print("*");
+	    
+	    for (int j=1; j<=tamaño-2; j++) {
+	    	
+	    	System.out.print(" ");
+	    	
+	    }
+	    System.out.println("*");
+	    
+	    }
+	    for (int i =1; i<=tamaño; i++) {
+	    	System.out.print("*");
+	    }
+	    
 		}
 	}
 
-}
+
